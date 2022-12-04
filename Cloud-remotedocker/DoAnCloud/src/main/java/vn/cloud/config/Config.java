a9a15199a1eb334f8ebeb49d9fb1bd3f8a2e8789
VoVanDuc20110635 <20110635@student.hcmute.edu.vn>
2022-12-04 13:26:25 +0700

    update project cloud

---
+++ Cloud-remotedocker/DoAnCloud/src/main/java/vn/cloud/config/Config.java
@@ -0,0 +1,24 @@
+package vn.cloud.config;
+
+public class Config {
+	
+	  public static String ipServer1 = "34.239.76.72"; // thay đổi ip bằng ip public trên ec2 chứa database 
+	  public static String userSql = "SA"; // thay đổi tài khoản đăng nhập sql container 
+	  public static String pasSql = "MatKhau*123"; // thay đổi mật khẩu đăng nhập sql container //
+	  //public static String privatekeyPath = "C:\\Users\\tranc\\cloud\\newubuntu1.pem"; // thay đổi path của key ec2 
+	  public static String privatekeyPath = "/usr/local/tomcat/webapps/DoAnCloud.pem"; 
+	  public static String databaseName = "usercloud";
+	 
+
+	/*
+	 * public static String ipServer1 = "DESKTOP-V2MH4TM"; // thay đổi ip bằng ip
+	 * public trên ec2 chứa database public static String userSql = "sa"; // thay
+	 * đổi tài khoản đăng nhập sql container public static String pasSql =
+	 * "duc2112002"; // thay đổi mật khẩu đăng nhập sql container //public static
+	 * String privatekeyPath = "C:\\Users\\tranc\\cloud\\newubuntu1.pem"; // thay
+	 * đổi path của key ec2 public static String privatekeyPath =
+	 * "/usr/local/tomcat/webapps/DoAnCloud.pem"; public static String databaseName
+	 * = "usercloud";
+	 */
+
+}
